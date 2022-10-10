9f394fa0db5ebd081a411eb6b9557310a3f18cf8
pwendy2015 <112223673+pwendy2015@users.noreply.github.com>
2022-10-10 01:23:18 -0400

    insta 1

---
+++ Instagram/Instagram/PosterCell.swift
@@ -0,0 +1,23 @@
+//
+//  PosterCell.swift
+//  Instagram
+//
+//  Created by Cleff Paillant on 10/8/22.
+//
+
+import UIKit
+
+class PosterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
